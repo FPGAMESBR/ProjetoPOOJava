d43508d1e5fd7fa85db55e5c778afb8e49c04b7f
Franklin P. Barboza <139097233+FPGAMESBR@users.noreply.github.com>
2024-03-06 19:42:05 -0300

    Main

--- Main.java
+++ Main.java
@@ -1,2 +1,23 @@
-public class Main {
+public class Aluno {
+    private String nome;
+    private String cpf;
+    private String rg;
+
+    // Construtor
+    public Aluno(String nome, String cpf, String rg) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    // Métodos para acessar e modificar as propriedades
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Outros métodos para manipular os dados do aluno
 }
