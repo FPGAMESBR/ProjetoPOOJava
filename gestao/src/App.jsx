0915f206b72b98f8648cbdd8d3c6ff862d20f129
Mayron28br <89049286+Mayron28br@users.noreply.github.com>
2024-05-06 15:35:57 -0300

    Update App.jsx

--- gestao/src/App.jsx
+++ gestao/src/App.jsx
@@ -1,21 +1,7 @@
-import './app.css';
-import HomePage from './pages/home/HomePage';
-import YearPage from './pages/years/YearPage';
-import TeacherPage from './pages/teachers/TeacherPage';
-import DiscPage from './pages/disciplines/DiscPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/years" element={<YearPage />} />
-        <Route path="/teachers" element={<TeacherPage />} />
-        <Route path="/disc" element={<DiscPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+
+function App() {
+
+}
 
 export default App;
