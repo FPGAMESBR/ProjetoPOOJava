228c3bf54eb97415f549aa69863e8e2ed6d59516
Franklin Pereira Barboza <139097233+FPGAMESBR@users.noreply.github.com>
2024-05-07 11:58:53 -0300

    de novo
    mayron fez besteira novamente

--- gestao/src/App.jsx
+++ gestao/src/App.jsx
@@ -1,7 +1,21 @@
+import './app.css';
+import HomePage from './pages/home/HomePage';
+import YearPage from './pages/years/YearPage';
+import TeacherPage from './pages/teachers/TeacherPage';
+import DiscPage from './pages/disciplines/DiscPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-function App() {
-
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/years" element={<YearPage />} />
+        <Route path="/teachers" element={<TeacherPage />} />
+        <Route path="/disc" element={<DiscPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
