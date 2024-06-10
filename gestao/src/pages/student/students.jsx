fbbdc238125b93a4a694343e43766bc66844b7c3
Bruno <147844144+BrunoAraujoBarros@users.noreply.github.com>
2024-06-10 16:33:10 -0300

    aiaia

--- gestao/src/pages/student/students.jsx
+++ gestao/src/pages/student/students.jsx
@@ -3,13 +3,88 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './students.css';
 
-function StudentButton({ name, bithdate, average, onClick }) {
+function StudentButton({ nomeAluno, dataNascimento, media, notas, onClick }) {
     return (
-        <button className='student' onClick={onClick}>
-            <p>{name}</p>
-            <p>{bithdate}</p>
-            <p>{average}</p>
-        </button>
+        <div className='student'>
+            <p>{nomeAluno}</p>
+            <p>{dataNascimento}</p>
+            <p>{media}</p>
+            <button onClick={() => onClick(notas)}>Detalhes</button>
+        </div>
+    );
+}
+
+function displayNotes(notasArray) {
+    return (
+        <div>
+            <h2>Notas</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Matéria</th>
+                        <th>Média</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {notasArray.map((nota, index) => (
+                        <React.Fragment key={index}>
+                            <tr>
+                                <td>Geografia</td>
+                                <td>{nota.geografia}</td>
+                            </tr>
+                            <tr>
+                                <td>Arte</td>
+                                <td>{nota.arte}</td>
+                            </tr>
+                            <tr>
+                                <td>Redação</td>
+                                <td>{nota.redacao}</td>
+                            </tr>
+                            <tr>
+                                <td>Religião</td>
+                                <td>{nota.religiao}</td>
+                            </tr>
+                            <tr>
+                                <td>Inglês</td>
+                                <td>{nota.ingles}</td>
+                            </tr>
+                            <tr>
+                                <td>Português</td>
+                                <td>{nota.portugues}</td>
+                            </tr>
+                            <tr>
+                                <td>História</td>
+                                <td>{nota.historia}</td>
+                            </tr>
+                            <tr>
+                                <td>Ciências</td>
+                                <td>{nota.ciencias}</td>
+                            </tr>
+                            <tr>
+                                <td>Geometria</td>
+                                <td>{nota.geometria}</td>
+                            </tr>
+                            <tr>
+                                <td>Filosofia</td>
+                                <td>{nota.filosofia}</td>
+                            </tr>
+                            <tr>
+                                <td>Educação Física</td>
+                                <td>{nota.efisica}</td>
+                            </tr>
+                            <tr>
+                                <td>Matemática</td>
+                                <td>{nota.matematica}</td>
+                            </tr>
+                            <tr>
+                                <td>Direito e Cidadania</td>
+                                <td>{nota.direitoCidadania}</td>
+                            </tr>
+                        </React.Fragment>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
@@ -18,7 +93,28 @@ function StudentInfo({ student }) {
 
     return (
         <div className='info'>
-            {/* Renderizar todas as informações do aluno aqui */}
+            <p>CPF: {student.cpf}</p>
+            <p>RG do Aluno: {student.rgAluno}</p>
+            <p>NIS: {student.nis}</p>
+            <p>Número da Certidão: {student.numeroCertidao}</p>
+            <p>Série/Ano: {student.serieAno}</p>
+            <p>Nome do Aluno: {student.nomeAluno}</p>
+            <p>Status do Aluno: {student.alunoStatus}</p>
+            <p>Naturalidade (Estado): {student.naturnalidadeEstado}</p>
+            <p>Nacionalidade: {student.nacionalidade}</p>
+            <p>Tipo Sanguíneo: {student.tipoSanguinio}</p>
+            <p>Gênero: {student.genero}</p>
+            <p>Nome do Pai: {student.nomePai}</p>
+            <p>Nome da Mãe: {student.nomeMae}</p>
+            <p>E-mail do Responsável: {student.emailResposavel}</p>
+            <p>CPF do Pai: {student.cpfPai}</p>
+            <p>CPF da Mãe: {student.cpfMae}</p>
+            <p>RG do Pai: {student.rgPai}</p>
+            <p>RG da Mãe: {student.rgMae}</p>
+            <p>Endereço: {student.endereco}</p>
+            <p>Data de Cadastro: {student.dataCadastro}</p>
+            <p>Data de Nascimento: {student.dataNascimento}</p>
+            {displayNotes(student.notas)}
         </div>
     );
 }
@@ -29,8 +125,7 @@ function Students() {
     const [selectedStudent, setSelectedStudent] = useState(null);
 
     useEffect(() => {
-        // Fetch students based on serieAluno
-        axios.get(`http://localhost:8080/api/alunos/${serieAluno}`)
+        axios.get(`http://localhost:8080/api/alunos/`)
             .then(response => {
                 setStudents(response.data);
             })
@@ -38,6 +133,10 @@ function Students() {
                 console.error('There was an error!', error);
             });
     }, [serieAluno]);
+
+    const handleStudentDetails = (notas) => {
+        setSelectedStudent({ ...selectedStudent, notas });
+    };
 
     return (
         <div className='app'>
@@ -49,13 +148,14 @@ function Students() {
                         <p>Nascimento</p>
                         <p>Média</p>
                     </div>
-                    {students.map((student, index) => (
+                    {students.map(student => (
                         <StudentButton
-                            key={index}
-                            name={student.name}
-                            bithdate={student.bithdate}
-                            average={student.average}
-                            onClick={() => setSelectedStudent(student)}
+                            key={student.cpf}
+                            nomeAluno={student.nomeAluno}
+                            dataNascimento={student.dataNascimento}
+                            media={student.notas.media}
+                            notas={student.notas}
+                            onClick={handleStudentDetails}
                         />
                     ))}
                 </div>
