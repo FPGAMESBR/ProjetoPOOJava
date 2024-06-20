2e6ef2159affe08e7648f2671b28f72d1f7078ab
Franklin P. Barboza <139097233+FPGAMESBR@users.noreply.github.com>
2024-06-19 23:22:07 -0300

    nota (rota indefinida)

--- gestao/src/pages/disciplines/grade.jsx
+++ gestao/src/pages/disciplines/grade.jsx
@@ -1,113 +1,121 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import axios from 'axios';
 import './grade.css';
 
-function GradeDisc() {
-    const { subject } = useParams();
+function GradesPage() {
+  const [students, setStudents] = useState([]);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+  const [grades, setGrades] = useState({});  
+  const [modifiedGrades, setModifiedGrades] = useState({});
 
-    // Estado para armazenar os alunos e as notas dos alunos
-    const [students, setStudents] = useState([]);
-    const [studentGrades, setStudentGrades] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/alunos/')
+      .then(response => {
+        setStudents(response.data);
+      })
+      .catch(error => console.error('Erro ao buscar alunos:', error));
+  }, []);
 
-    useEffect(() => {
-        axios.get(`http://localhost:8080/api/alunos/`)
-            .then(response => {
-                const studentsData = response.data;
-                const initialStudentGrades = studentsData.map(student => ({
-                    cpf: student.cpf,
-                    nomeAluno: student.nomeAluno,
-                    serieAno: student.serieAno,
-                    genero: student.genero,
-                    notas: {
-                        portugues: parseFloat(student.notas?.find(nota => nota.materia === 'portugues')?.media) || 0,
-                        matematica: parseFloat(student.notas?.find(nota => nota.materia === 'matematica')?.media) || 0,
-                        ingles: parseFloat(student.notas?.find(nota => nota.materia === 'ingles')?.media) || 0,
-                        // Adicione outras disciplinas conforme necessário
-                    }
-                }));
-                setStudents(studentsData);
-                setStudentGrades(initialStudentGrades);
-            })
-            .catch(error => console.error('Erro ao buscar alunos:', error));
-    }, []);
+  const handleStudentClick = (student) => {
+    setSelectedStudent(student);
+    axios.get(`http://localhost:8080/api/classes/notas-alunos/${student.cpf}`) //back sem essa rota
+      .then(response => {
+        console.log(`Notas do aluno ${student.cpf}:`, response.data);
+        const studentGrades = response.data;
+        if (studentGrades) {
+          setGrades(studentGrades);
+        } else {
+          setGrades({});
+        }
+      })
+      .catch(error => console.error(`Erro ao buscar notas para o aluno ${student.cpf}:`, error));
+  };   
 
-    // Função para atualizar a nota de um aluno para uma disciplina específica
-    const updateGrade = (index, value) => {
-        const updatedGrades = [...studentGrades];
-        updatedGrades[index].notas[subject.toLowerCase()] = parseFloat(value);
-        setStudentGrades(updatedGrades);
-    };
+  const handleGradeChange = (subject, value) => {
+    setModifiedGrades(prevModifiedGrades => ({
+      ...prevModifiedGrades,
+      [subject]: parseFloat(value)
+    }));
+  };
 
-    // Função para enviar as notas para o backend usando PUT
-    const sendGrades = () => {
-        studentGrades.forEach(student => {
-            const { cpf, notas } = student;
+  const sendGrades = () => {
+    if (selectedStudent) {
+      axios.put(`http://localhost:8080/api/classes/editar-notas/${selectedStudent.cpf}`, {
+        cpfAluno: selectedStudent.cpf,
+        ...modifiedGrades
+      })
+        .then(response => {
+          console.log(`Notas atualizadas com sucesso para o CPF ${selectedStudent.cpf}:`, response.data);
+          setSelectedStudent(prevSelectedStudent => ({
+            ...prevSelectedStudent,
+            notas: { ...prevSelectedStudent.notas, ...modifiedGrades }
+          }));
+        })
+        .catch(error => console.error(`Erro ao atualizar notas para o CPF ${selectedStudent.cpf}:`, error));
+      setModifiedGrades({});
+    }
+  };
 
-            // Verifica se a nota da disciplina específica é maior que 0.1 antes de enviar
-            const notaDisciplina = notas[subject.toLowerCase()];
-            if (notaDisciplina > 0.1) {
-                const payload = { [subject.toLowerCase()]: notaDisciplina };
-                axios.put(`http://localhost:8080/api/classes/editar-notas/${cpf}`, payload)
-                    .then(response => {
-                        console.log(`Notas atualizadas com sucesso para o CPF ${cpf}:`, response.data);
-
-                        // Atualiza apenas a nota da disciplina específica no estado local
-                        const updatedStudentGrades = [...studentGrades];
-                        const updatedStudentIndex = updatedStudentGrades.findIndex(s => s.cpf === cpf);
-                        updatedStudentGrades[updatedStudentIndex].notas[subject.toLowerCase()] = response.data[subject.toLowerCase()];
-                        setStudentGrades(updatedStudentGrades);
-                    })
-                    .catch(error => console.error(`Erro ao atualizar notas para o CPF ${cpf}:`, error));
-            } else {
-                console.log(`Notas não foram atualizadas para o CPF ${cpf} porque são menores ou iguais a 0.1`);
-            }
-        });
-    };
-
-    return (
-        <div className="grade-students-page">
-            <header className="Header-grades">
-                <h1>Notas dos Alunos</h1>
-            </header>
-            <div className="list-students-grades">
-                <h1>Atribuir notas para {subject}</h1>
-
-                <div className="give-grade-list">
-                    <div className="give-grade-list-header">
-                        <p>Selecionar</p>
-                        <p>Nome</p>
-                        <p>Série</p>
-                        <p>CPF</p>
-                        <p>Sexo</p>
-                        <p>Nota</p>
-                    </div>
-                    {studentGrades.map((student, index) => (
-                        <div className="student-grade" key={index}>
-                            <label className="custom-checkbox">
-                                <input name="dummy" type="checkbox" />
-                                <span className="checkmark"></span>
-                            </label>
-                            <p>{student.nomeAluno}</p>
-                            <p>{student.serieAno}</p>
-                            <p>{student.cpf}</p>
-                            <p>{student.genero}</p>
-                            <input 
-                                className="grade-input" 
-                                placeholder="Nota"
-                                name={subject.toLowerCase()} 
-                                type="number"
-                                step="0.1"
-                                value={student.notas[subject.toLowerCase()]} // Acesso direto à nota da disciplina específica
-                                onChange={(e) => updateGrade(index, e.target.value)} 
-                            />
-                        </div>
-                    ))}
-                </div>
-                <button className="button-send-grades" onClick={sendGrades}>Enviar Notas</button>
+  return (
+    <div className="grades-page">
+      <header className="Header-grades">
+        <h1>Notas dos Alunos</h1>
+      </header>
+      <div className="list-students-grades">
+        <h1>Lista de Alunos</h1>
+        <div className="students-list">
+          {students.map(student => (
+            <div
+              key={student.cpf}
+              className={`student ${selectedStudent?.cpf === student.cpf ? 'selected' : ''}`}
+              onClick={() => handleStudentClick(student)}
+            >
+              <p>{student.nomeAluno}</p>
+              <p>{student.serieAno}</p>
+              <p>{student.cpf}</p>
+              <p>{student.genero}</p>
             </div>
+          ))}
         </div>
-    );
+      </div>
+      {selectedStudent && (
+        <div className="edit-grades">
+          <h1>Editar Notas de {selectedStudent.nomeAluno}</h1>
+          <div className="give-grade-list">
+          {[
+            'portugues',
+            'matematica',
+            'ingles',
+            'redacao',
+            'ciencias',
+            'historia',
+            'geografia',
+            'filosofia',
+            'arte',
+            'efisica',
+            'direitoCidadania',
+            'religiao',
+            'geometria' 
+            ].map(subject => (
+            <div key={subject} className="grade-input-container">
+                <label className="subject-label">{subject}</label>
+                <input 
+                    className="grade-input" 
+                    placeholder="Nota"
+                    name={subject} 
+                    type="number"
+                    step="0.1"
+                    value={grades[subject] || ''}
+                    onChange={(e) => handleGradeChange(subject, e.target.value)} 
+                />
+            </div>
+            ))}
+          </div>
+          <button className="button-send-grades" onClick={sendGrades}>Enviar Notas</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default GradeDisc;
+export default GradesPage;
